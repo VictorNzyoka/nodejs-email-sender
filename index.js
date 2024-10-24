e586a3a1dbc73398c7d9fc9e981e0f38b77de046
VictorNzyoka <mutunga.victor21@students.dkut.ac.ke>
2024-10-24 11:09:35 +0300

    cors

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const dotenv = require('dotenv');
+const cors = require('cors');
 const emailRoutes = require('./routes/emailRoutes');
 
 // Initialize dotenv to load environment variables from .env
 dotenv.config();
 
 const app = express();
+
+// Enable CORS for all requests
+app.use(cors());
 
 // Middleware to parse incoming form data
 app.use(bodyParser.urlencoded({ extended: true }));
